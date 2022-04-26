91d65731c0d47673c2ecab9904d1e851006deb8c
Shota Shimazu <hornet.live.mf@gmail.com>
2022-04-26 18:43:10 +0900

    add initializer

--- initializer.go
+++ initializer.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/user"
+)
+
+const G_STASH_FILE = ".gstash.db"
+
 func Init() {
 	println("Initializing...")
+
+	u, err := user.Current()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	fmt.Println(u.HomeDir)
+
 	println("Done!")
 }
