ba0639ee16957cd0f45d7ff09f4f1f4566d786c2
Shota Shimazu <hornet.live.mf@gmail.com>
2022-07-15 16:49:03 +0900

    replace SQLite in GRDB

--- Sources/StashKit/Database.swift
+++ Sources/StashKit/Database.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import GRDB
 
 
 let dbFile = "stash.db"
@@ -18,6 +19,8 @@ let fileManager = FileManager.default
 
 open class StashDatabase {
 
+    let dbQueue: DatabaseQueue
+    let keyQueue: DatabaseQueue
 
     public init?() {
 
@@ -25,10 +28,23 @@ open class StashDatabase {
             Logger.log("LOAD DATABASE \(workingDir)/\(dbFile)")
             Logger.log("LOAD DATABASE \(workingDir)/\(keys)")
 
-            
+            dbQueue = try DatabaseQueue(path: "\(workingDir)/\(dbFile)")
+            keyQueue = try DatabaseQueue(path: "\(workingDir)/\(keys)")
         } catch {
             Logger.log("FAILED TO ESTABLISH CONNECTIONS!")
             return nil
+        }
+    }
+
+    func prepareSchema() throws {
+        try dbQueue.write { db in
+            try db.create(table: "stashes") { t in
+                t.autoIncrementedPrimaryKey("id")
+                t.column("name", .text).notNull()
+                t.column("content", .text).notNull()
+                t.column("is_encrypted", .boolean).notNull()
+
+            }
         }
     }
 }
